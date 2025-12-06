932253e207d32702aabe5be305f240833834a235
Artyom Sokolov <48649189+kroflex1@users.noreply.github.com>
2025-12-06 15:25:06 +0500

    добавлена пагинация в часть запросов

--- src/main/java/org/example/schoolback/repository/GroupRepository.java
+++ src/main/java/org/example/schoolback/repository/GroupRepository.java
@@ -2,6 +2,8 @@ package org.example.schoolback.repository;
 
 import org.example.schoolback.entity.Group;
 import org.example.schoolback.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,4 +23,12 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
             "WHERE pg.user_id = :userId",
             nativeQuery = true)
     List<Group> findAllGroupsByStudentId(@Param("userId") Long studentId);
+
+    Page<Group> findByTeacherId(Long teacherId, Pageable pageable);
+
+    @Query(value = "SELECT g.* FROM groups g " +
+            "JOIN participants_groups pg ON g.id = pg.group_id " +
+            "WHERE pg.user_id = :userId",
+            nativeQuery = true)
+    Page<Group> findAllGroupsByStudentId(@Param("userId") Long studentId, Pageable pageable);
 }
